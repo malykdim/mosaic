7ca775a4cfa94eecce359e63ab7003fb21220f45
Albena Kostova <albena.kostov@gmail.com>
2024-12-07 22:21:44 +0100

    Adding Auth to the Navbar

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,5 +1,19 @@
 <script setup>
+import { useRouter } from 'vue-router'
 
+import useLogout from '../../composables/useLogout'
+import getUser from '../../composables/getUser'
+
+const router = useRouter()
+
+const { user } = getUser()
+const { logout } = useLogout()
+
+const logOut = async () => {
+    const response = await logout()
+    console.log('user logged out')
+    router.push({ name: 'login' })
+}
 </script>
 
 <template>
@@ -8,6 +22,11 @@
             <img src="/logo.png" class="logo" alt="Logo" />
         </router-link>
         <router-link :to="{ name: 'gallery'}" class="link btn">Gallery</router-link>
+        <div class="auth">
+            <router-link v-if="!user" :to="{ name: 'login'}" class="link btn">Login</router-link>
+            <router-link v-if="!user" :to="{ name: 'signup'}" class="link btn">Register</router-link>
+            <button v-if="user" @click="logOut" class="link btn">Logout</button>
+        </div>
     </nav>
 </template>
 
@@ -19,6 +38,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 5rem;
 
     // padding: 0; 
     padding: var(--safe-area-inset-top) var(--safe-area-inset-right) var(--safe-area-inset-bottom) var(--safe-area-inset-left);   
@@ -54,6 +74,17 @@
         &:hover {
             color: white;
             background: var(--primary);
+        }
+    }
+
+    .auth {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: auto;
+
+        .link {
+            margin: 0 0.5em;
         }
     }
 }
