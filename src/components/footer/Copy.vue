eade07fe2782e212431601cd45b575539e749181
Albena Kostova <albena.kostov@gmail.com>
2025-08-17 22:23:16 +0200

    refactor(footer): update components and cleanup

--- src/components/footer/Copy.vue
+++ src/components/footer/Copy.vue
@@ -3,13 +3,12 @@
 </script>
 
 <template>
-<p>damyanovi &copy; 2024</p>
+    <p class="copy">The Stone magic &copy; 2024</p>
 </template>
 
 <style lang='scss' scoped>
-p {
+.copy {
     font-family: 'M PLUS 1p', sans-serif;
-
     color: var(--text, white); // fallback to white if --text not set
     font-size: 1em;
     opacity: 1;
--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Copy from './Copy.vue'
-import Auth from './Auth.vue'
 </script>
 
 <template>
     <div class="footer-content">
         <Copy />
-        <Auth />
     </div>
 </template>
