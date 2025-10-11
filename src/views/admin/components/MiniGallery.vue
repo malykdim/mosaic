90cd3aebb38041e920fedd23898347eceb9a7607
Albena Kostova <albena.kostov@gmail.com>
2025-10-11 21:16:02 +0200

    Dashboard style adjustments

--- src/views/admin/components/MiniGallery.vue
+++ src/views/admin/components/MiniGallery.vue
@@ -2,17 +2,35 @@
 import { onMounted } from 'vue'
 import { useGalleryListener } from '../../../stores/useGalleryListener.js'
 import MiniItem from './MiniItem.vue'
-
-const { mosaics, startListening } = useGalleryListener()
+import ImageSkeleton from '../../../components/ImageSkeleton.vue'
+const { mosaics, isLoading, error, startListening } = useGalleryListener()
 
 onMounted(() => startListening())
 
 </script>
 
 <template>
-<div class="mini-gallery">
-    <div  v-if="mosaics.length" class="items">
-      <MiniItem v-for="item in mosaics" :key="item.id" :item="item" />
-    </div>
-</div>
+  <div class="mini-gallery">
+      <!-- Show error if it exists -->
+      <div v-if="error" class="error">
+        Не можахме да заредим галерията. Моля, опитайте отново.
+      </div>
+
+      <!-- Show skeletons while loading -->
+      <div  v-if="isLoading" class="items">
+        <div v-for="n in 6" :key="`skeleton-${n}`" class="item">
+            <ImageSkeleton />
+        </div>
+      </div>
+
+      <!-- Show actual content when loaded -->
+      <div  v-if="mosaics.length" class="items">
+        <MiniItem v-for="item in mosaics" :key="item.id" :item="item" />
+      </div>
+
+      <!-- Show empty state if no items -->
+      <div v-else class="empty-state">
+          Все още няма мозайки в галерията.
+      </div>
+  </div>
 </template>
--- src/views/gallery/Gallery.vue
+++ src/views/gallery/Gallery.vue
@@ -14,14 +14,14 @@ startListening()
         <div v-if="error" class="error">Не можахме да заредим галерията. Моля, опитайте отново.</div>
 
         <!-- Show skeletons while loading -->
-        <div v-else-if="isLoading" class="items">
+        <div v-if="isLoading" class="items">
             <div v-for="n in 6" :key="`skeleton-${n}`" class="item">
                 <ImageSkeleton />
             </div>
         </div>
 
         <!-- Show actual content when loaded -->
-        <div  v-else-if="mosaics.length" class="items">
+        <div  v-if="mosaics.length" class="items">
             <div v-for="item in mosaics" :key="item.id" class="item">
                 <ListItem :item="item" />
             </div>
